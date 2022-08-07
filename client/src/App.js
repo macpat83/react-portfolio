13e83fd298604958edcdddb95537e0fa2fa34125
macpat83 <macpat@gmail.com>
2022-08-07 16:46:25 -0500

    added resume component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
